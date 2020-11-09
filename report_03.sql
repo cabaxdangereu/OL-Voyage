501731d04bd20393e72fffecb7512ab04966b0ff
VERGOZ Dimitri <dimitri.vergoz@edu.devinci.fr>
2020-11-09 19:21:06 +0000

    report_03.sql

--- report_03.sql
+++ report_03.sql
@@ -6,4 +6,5 @@ ON resa.reservation_id = emp.employee_id
 JOIN T_CUSTOMER custom
 ON resa.buyer_id = custom.customer_id
 GROUP BY resa.reservation_id, resa.creation_date, emp.last_name, emp.first_name, custom.last_name, custom.first_name)
-WHERE ROWNUM = 1;
+WHERE ROWNUM = 1
+/
