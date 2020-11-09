e4d98780af46a4979acfd90283fcc350c644db82
VERGOZ Dimitri <dimitri.vergoz@edu.devinci.fr>
2020-11-09 19:20:21 +0000

    report_02.sql

--- report_02.sql
+++ report_02.sql
@@ -1,6 +1,7 @@
-SELECT DISTINCT UPPER(custom .last_name) || ‘ ‘ || custom .first_name "NOM / Prénom du client"
+SELECT DISTINCT UPPER(custom .last_name) || ' ' || custom .first_name "NOM / Prnom du client"
 FROM T_CUSTOMER custom JOIN T_RESERVATION resa
 ON resa.buyer_id = custom.customer_id JOIN T_TICKET ticket
 ON ticket.reservation_id = resa.reservation_id
 WHERE resa.buyer_id != ticket.customer_id
-ORDER BY "NOM / Prénom du client";
+ORDER BY "NOM / Prenom du client"
+/
