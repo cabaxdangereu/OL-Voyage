37c4528683870ce6c860d9946c1a304cd031bc10
VERGOZ Dimitri <dimitri.vergoz@edu.devinci.fr>
2020-11-11 23:12:11 +0000

    report_01.sql edited online with Bitbucket

--- report_01.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM(SELECT UPPER(emp.last_name) || ' ' || emp.first_name "NOM/PRENOM de l'employee"
-FROM T_EMPLOYEE emp
-JOIN T_RESERVATION resa
-ON resa.employee_id = emp.employee_id
-GROUP BY emp.last_name, emp.first_name
-ORDER BY COUNT(resa.reservation_id)DESC)
-WHERE ROWNUM = 1
-/
