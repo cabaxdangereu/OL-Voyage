03a8561e67b95163b91878a308a451c8939ead43
VERGOZ Dimitri <dimitri.vergoz@edu.devinci.fr>
2020-11-11 23:13:30 +0000

    report_05.sql edited online with Bitbucket

--- report_05.sql
+++
@@ -1,9 +0,0 @@
-SELECT traininfo.train_id "Numero du train ", depart.city || TO_CHAR(traininfo.departure_time, ' (DD/MM/YY HH24:MI) ') ||' - '|| arrivee.city ||
-TO_CHAR(traininfo.arrival_time, ' (DD/MM/YY HH24:MI) ') " Trajet ", traininfo.distance " Distance", traininfo.price || '' " Prix initial "
-FROM t_train traininfo
-JOIN t_station depart
-ON traininfo.departure_station_id = depart.station_id
-JOIN t_station arrivee
-ON traininfo.arrival_station_id = arrivee.station_id
-ORDER BY traininfo.train_id ASC
-/
