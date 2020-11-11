75e9af2f9c0a9379aee8a31964fc6981a5e77bc4
VERGOZ Dimitri <dimitri.vergoz@edu.devinci.fr>
2020-11-11 23:13:50 +0000

    report_12.sql edited online with Bitbucket

--- report_12.sql
+++
@@ -1,6 +0,0 @@
-SELECT pass.pass_name "Nom du pass", COUNT(client.pass_id)"Nombre de pass"
-FROM T_PASS pass
-JOIN T_CUSTOMER client
-ON pass.pass_id = client.pass_id
-GROUP BY client.pass_id, pass.pass_name
-ORDER BY COUNT(client.pass_id)desc;
