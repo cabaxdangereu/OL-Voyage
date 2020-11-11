fb39dbdb33bf1464e99aaeceb5cabf62539c50d9
VERGOZ Dimitri <dimitri.vergoz@edu.devinci.fr>
2020-11-11 23:24:17 +0000

    report_04.sql created online with Bitbucket

---
+++ sql/report_04.sql
@@ -0,0 +1,10 @@
+SELECT UPPER(custom .last_name) || ' ' || custom .first_name "NOM / Prenom du client",
+CASE
+WHEN (MONTHS_BETWEEN('14/10/2020', custom.pass_date)>12) THEN 'Perime !'
+WHEN (MONTHS_BETWEEN('14/10/2020', custom.pass_date) is NULL) THEN '-'
+END "Pass"
+FROM t_customer custom
+LEFT OUTER JOIN t_pass pass
+ON (custom.pass_id = custom.pass_id)
+ORDER BY "NOM / Prenom du client" 
+/
