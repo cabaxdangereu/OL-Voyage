2b63ff642700eb25b33a76a99cad22dad4035502
VERGOZ Dimitri <dimitri.vergoz@edu.devinci.fr>
2020-11-11 23:12:29 +0000

    report_15.sql edited online with Bitbucket

--- report_15.sql
+++
@@ -1,9 +0,0 @@
-SELECT UPPER(e.last_name) || ' ' || e.first_name "NOM Pr‚nom",
- COUNT(r.reservation_id) "Nb de resa"
-FROM T_EMPLOYEE e
-LEFT OUTER JOIN T_RESERVATION r
-ON e.employee_id = r.employee_id
-WHERE manager_id IS NOT NULL
-GROUP BY manager_id, r.employee_id, e.last_name, e.first_name
-ORDER BY "Nb de resa" DESC
-/
