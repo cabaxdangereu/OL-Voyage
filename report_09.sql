0c2bdd05796d23dd2065f957b36c645042635509
VERGOZ Dimitri <dimitri.vergoz@edu.devinci.fr>
2020-11-11 23:14:03 +0000

    report_09.sql edited online with Bitbucket

--- report_09.sql
+++
@@ -1,8 +0,0 @@
-SELECT train.train_id "Numero Train", ROUND(train.distance/((train.arrival_time-train.departure_time)*24), 0) ||' km/h' "Vitesse Moyenne",depart.city ||' - '|| arrivee.city "Depart - Arrivee"
-FROM t_train train
-JOIN t_station depart
-ON (train.departure_station_id=depart.station_id)
-JOIN t_station arrivee
-ON (train.arrival_station_id=arrivee.station_id)
-ORDER BY train_id;
-
